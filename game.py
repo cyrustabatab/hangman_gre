ac5e247776c9e3e6538ea0a485680ed20d01c0c5
Cyrus Taba <24766642+cyrustabatab@users.noreply.github.com>
2021-08-20 17:43:21 -0700

    add jumble mode

--- game.py
+++ game.py
@@ -74,6 +74,11 @@ class Menu:
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_1:
+                        Game(jumble_mode=False)
+                    elif event.key == pygame.K_2:
+                        Game(jumble_mode=True)
 
             
             screen.fill(WHITE)
@@ -87,18 +92,21 @@ class Game:
 
     line_thickness = 4 
     font = pygame.font.SysFont("calibri",40,bold=True)
-    def __init__(self,words="words.csv"):
+    def __init__(self,words="words.csv",jumble_mode=True):
         
         self.definitions = pd.read_csv(words,index_col=0)
         self.words = self.definitions.index
-
+        self.jumble_mode = jumble_mode
         self.word_text = self.font.render("WORD:",True,BLACK)
         self.guess_text = self.font.render("GUESS:",True,BLACK)
+        self.score = 0
         self.font.set_underline(True)
-        self.letters_guessed_text = self.font.render("LETTERS GUESSED",True,BLACK)
+        self.letters_guessed_text = self.font.render(f"{'LETTERS' if not self.jumble_mode else 'WORDS'} GUESSED",True,BLACK)
         self.letter_used_start = None
         self.font.set_underline(False)
-        self.letter_already_used_text = self.font.render("LETTER ALREADY GUESSED",True,RED)
+        self.letter_already_used_text = self.font.render(f"{'LETTER' if not self.jumble_mode else 'WORDS'} ALREADY GUESSED",True,RED)
+
+
         self.you_win_text = self.font.render("YOU GUESSED IT!",True,GREEN)
         self.hint_button = pygame.sprite.GroupSingle(Button("HINT",None,250,BLACK,WHITE,self.font,centered_x=True))
         self.score_text = self.font.render("0",True,BLACK)
@@ -149,16 +157,25 @@ class Game:
     def _setup(self):
 
         self.word = self._choose_word()
+
+        if self.jumble_mode:
+            word_list = list(self.word)
+            random.shuffle(word_list)
+            self.shuffled_word = [self.font.render(character,True,BLACK) for character in word_list]
+            self.length_start_time = None
+
         self.guesses = [None] * len(self.word)
         self.definition_text = self.font.render(self.definitions.loc[self.word,'definition'],True,BLACK)
         self.show_definition = False
+
+        if self.jumble_mode:
+            self.length_error = self.font.render(f"WORD IS {len(self.word)} CHARACTERS LONG!",True,RED)
         self.lives = 6
         self.guess = ''
         self.game_over = False
         self.user_guess_text =self.font.render(self.guess,True,BLACK)
         self.letters_used = []
         self.letters_used_text = None
-        self.score = 0
 
 
 
@@ -170,20 +187,28 @@ class Game:
 
 
         
+        
+
         left_x = 400
         y = 100
+
         screen.blit(self.word_text,(left_x - self.word_text.get_width() - 5,y - self.word_text.get_height()))
         gap = 20
         length = 25
+
+
         for i,guess in enumerate(self.guesses):
             pygame.draw.line(screen,BLACK,(left_x + gap +  i * (length + gap),y),(left_x + gap + i * (length + gap) + length,y),self.line_thickness)
-            if guess:
+            if guess or self.jumble_mode:
+                if self.jumble_mode and not self.game_over:
+                    guess = self.shuffled_word[i]
                 screen.blit(guess,(left_x + gap + i * (length + gap) + length//2 -  guess.get_width()//2,y - guess.get_height()))
 
 
 
         screen.blit(self.guess_text,(left_x - self.guess_text.get_width() - 5,y + 250))
         screen.blit(self.user_guess_text,(left_x + 20,y + 250))
+
         screen.blit(self.letters_guessed_text,(left_x,y + 50))
         
         if not self.game_over and self.lives <= 3:
@@ -198,51 +223,68 @@ class Game:
 
         if self.letter_used_start:
             screen.blit(self.letter_already_used_text,(SCREEN_WIDTH//2 - self.letter_already_used_text.get_width()//2,y + 350))
+        elif self.jumble_mode and self.length_start_time:
+            screen.blit(self.length_error,(SCREEN_WIDTH//2 - self.length_error.get_width()//2,y + 350))
         elif self.game_over:
             screen.blit(self.game_over_text,(SCREEN_WIDTH//2 - self.game_over_text.get_width()//2,y + 350))
 
         screen.blit(self.score_text,(SCREEN_WIDTH - self.score_text.get_width(),2))
 
+    
 
+    def _set_win(self):
+        self.game_over_text = self.you_win_text
+        self.score += 1
+        self.score_text = self.font.render(f"{self.score}",True,BLACK)
+        self.game_over = True
+        self.show_definition = False
+        self.game_over = True
     
     def _check_guess(self):
         
+
         if self.letter_used_start:
             self.letter_used_start = None
         
         if self.guess in self.letters_used:
             self.letter_used_start = time.time()
             return
-        found = False
-        for i,c in enumerate(self.word):
-            if c == self.guess:
-                self.guesses[i] = self.font.render(c,True,BLACK)
-                found = True
-        
+        if self.jumble_mode:
+            if self.length_start_time:
+                self.length_start_time = None
+            if len(self.guess) != len(self.word):
+                self.length_start_time = time.time()
+                return
 
+            if self.guess == self.word:
+                self._set_win()
+                self.guesses = [self.font.render(c,True,BLACK) for c in self.word]
+                return
+        else:
+            found = False
+            for i,c in enumerate(self.word):
+                if c == self.guess:
+                    self.guesses[i] = self.font.render(c,True,BLACK)
+                    found = True
+            
+
+
+            
+
+            if not found:
+                self.lives -= 1
+                if self.lives == 0:
+                    self._show_missed_guesses()
+                    self.game_over_start_time = time.time()
+                    self.game_over_text = self.font.render(f"Sorry! The word was {self.word}",True,RED)
+                    self.game_over = True
+                    self.show_definition = False
+            elif None not in self.guesses:
+                self._set_win()
+        
 
         self.letters_used.append(self.guess)
         self.letters_used_text = self.font.render(','.join(self.letters_used),True,BLACK)
-        
-
-        if not found:
-            self.lives -= 1
-            if self.lives == 0:
-                self._show_missed_guesses()
-                self.game_over_start_time = time.time()
-                self.game_over_text = self.font.render(f"Sorry! The word was {self.word}",True,RED)
-                self.game_over = True
-                self.show_definition = False
-        elif None not in self.guesses:
-                self.game_over_start_time = time.time()
-                self.game_over_text = self.you_win_text
-                self.score += 1
-                self.score_text = self.font.render(f"{self.score}",True,BLACK)
-                self.game_over = True
-                self.show_definition = False
-        
-                
-
 
         
     def _show_missed_guesses(self):
@@ -272,9 +314,10 @@ class Game:
                     sys.exit()
                 if event.type == pygame.KEYDOWN:
                     if not self.game_over:
-                        if not self.guess and pygame.K_a <= event.key <= pygame.K_z:
-                            self.guess += chr(event.key)
-                            self.user_guess_text = self.font.render(self.guess,True,BLACK)
+                        if pygame.K_a <= event.key <= pygame.K_z:
+                            if (not self.jumble_mode and not self.guess) or (self.jumble_mode):
+                                self.guess += chr(event.key)
+                                self.user_guess_text = self.font.render(self.guess,True,BLACK)
                         elif event.key == pygame.K_BACKSPACE:
                             self.guess = ''
                             self.user_guess_text = self.font.render(self.guess,True,BLACK)
@@ -298,11 +341,17 @@ class Game:
                 else:
                     self.show_definition = False
         
+            
 
-            if self.letter_used_start:
+
+            if self.letter_used_start or (self.jumble_mode and self.length_start_time):
                 current_time = time.time()
-                if current_time - self.letter_used_start >= 2:
-                    self.letter_used_start = None
+                if self.letter_used_start:
+                    if current_time - self.letter_used_start >= 2:
+                        self.letter_used_start = None
+                else:
+                    if current_time - self.length_start_time >= 2:
+                        self.length_start_time = None
 
 
 
@@ -331,8 +380,8 @@ class Game:
 if __name__ == "__main__":
 
 
-
-    Game()
+    Menu()
+    #Game()
 
 
 
